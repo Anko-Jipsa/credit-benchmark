57897f99d46a6d9d97598521b46a9db791e1f443
Joon Kang <jhkang@deloitte.co.uk>
2021-04-30 13:32:57 +0100

    Front cover update

--- pages/front.py
+++ pages/front.py
@@ -4,7 +4,8 @@ import streamlit as st
 def main():
     st.header("Front Page")
     st.markdown("""
-
+    **Welcome to the Benchmark analysis dashboard!**
+    
     To begin with, please select the item on your LHS panel. 
     
     Then select filters to retrieve the analysis.
